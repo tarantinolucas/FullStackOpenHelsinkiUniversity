5d05ed5345b7d5a54af99ee86369db3ea305aeab
Lucas Tarantino <tarantinolucas@hotmail.com>
2025-10-05 21:02:01 -0300

    Initialize part1 project with Vite, React

---
+++ part1/src/App.jsx
@@ -0,0 +1,17 @@
+const App = () => {
+  const now = new Date();
+  const a = 10;
+  const b = 20;
+  console.log("Hello from component");
+
+  return (
+    <div>
+      <p>Hello world, it is {now.toString()}</p>
+      <p>
+        {a} plus {b} is {a + b}
+      </p>
+    </div>
+  );
+};
+
+export default App;
